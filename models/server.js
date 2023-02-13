354c3f85b3c38066c786bd33ff1c5a803c1b014c
OnilBlanco26 <onilblanco26@gmail.com>
2023-02-13 16:10:48 -0300

    Server, constructor

--- models/server.js
+++ models/server.js
@@ -2,11 +2,14 @@ const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 const { db } = require('../database/db');
+const { userRouter } = require('../routes/users.routes');
+const { repairsRouter } = require('../routes/repairs.routes');
+const initModel = require('./initModels');
 
 class Server {
   constructor() {
     this.app = express();
-    this.port = 3000;
+    this.port = process.env.PORT || 3000;
 
     this.paths = {
       user: '/api/v2/user',
@@ -19,18 +22,27 @@ class Server {
   }
 
   middlewares() {
+    if (process.env.NODE_ENV === 'development') {
+      this.app.use(morgan('dev'));
+    }
+
     //UTILIZAMOS LAS CORS PARA PERMITIR ACCESO A LA API DESDE EL FRONT-END
     this.app.use(cors());
     //UTLIIZAMOS EXPRESS.JSON PARA PARSEAR EL BODY DE LA REQUEST
     this.app.use(express.json());
   }
 
-  routes() {}
+  routes() {
+    this.app.use(this.paths.user, userRouter);
+    this.app.use(this.paths.repairs, repairsRouter);
+  }
 
   database() {
     db.authenticate()
       .then(() => console.log('Database authenticate'))
       .catch(err => console.log(err));
+
+    initModel();
 
     db.sync()
       .then(() => console.log('Database Synced'))
