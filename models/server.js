3ee51c8759b8cf898876c6abacc12e4b95bd2b20
OnilBlanco26 <onilblanco26@gmail.com>
2023-02-27 13:32:53 -0300

    Attack management

--- models/server.js
+++ models/server.js
@@ -1,3 +1,4 @@
+const { rateLimit } = require('express-rate-limit');
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
@@ -7,11 +8,19 @@ const { repairsRouter } = require('../routes/repairs.routes');
 const initModel = require('./initModels');
 const globalErrorHandler = require('../controllers/error.controller');
 const AppError = require('../utils/appError');
+const { default: helmet } = require('helmet');
+const hpp = require('hpp');
+const xss = require('xss-clean')
 
 class Server {
   constructor() {
     this.app = express();
     this.port = process.env.PORT || 3000;
+    this.limiter = rateLimit({
+      max: 100,
+      windowMS: 15 * 60 * 1000,
+      message: "Too many request from this IP, please try again in 15 minutes"
+    })
 
     this.paths = {
       user: '/api/v2/user',
@@ -24,9 +33,16 @@ class Server {
   }
 
   middlewares() {
+    this.app.use(helmet())
+    this.app.use(xss())
+    this.app.use(hpp())
+
     if (process.env.NODE_ENV === 'development') {
       this.app.use(morgan('dev'));
     }
+    
+    // Este código se utiliza para limitar el número de llamadas API realizadas al servidor a 100 por hora.
+    this.app.use('/api/v2', this.limiter)
 
     //UTILIZAMOS LAS CORS PARA PERMITIR ACCESO A LA API DESDE EL FRONT-END
     this.app.use(cors());
